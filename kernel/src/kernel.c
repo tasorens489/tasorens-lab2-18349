434931f53a16c6dda641bcc22895ab2235fdaa48
Thomas Sorensen <tasorens@andrew.cmu.edu>
2026-09-17 09:08:04 -0400

    added gpio inits for the LED and BUttons(sections 6 and 7). They dont do anything yet but i dont think they have to for checkpoint

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -15,11 +15,38 @@
 
 int kernel_main() {
   //baud is set using a define so parameter doesn't matter
+  
+  //LEDR(A3 = PB_0)
+  gpio_init(GPIO_B, 0, MODE_GP_OUTPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_NONE, ALT0);
+  //set LEDR high for now
+  gpio_set(GPIO_B, 0);
+
+  //LEDG(D13 = PA_5)
+  gpio_init(GPIO_A, 5, MODE_GP_OUTPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_NONE, ALT0);
+  gpio_set(GPIO_A, 5);
+
+  //Button 1(D6 = PB_10)
+  gpio_init(GPIO_B, 10, MODE_INPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_PULL_UP, ALT0);
+  //Button 2(D4 = PB_5)
+  gpio_init(GPIO_B, 5, MODE_INPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_PULL_UP, ALT0);
+
+  //counter so gpio_isn't contantly printing to terminal so often
+  int counter = 0;
+
   uart_polling_init(USART_BRR);
   printk("hello world\n");
+
+  //counter is an arbitrary number of cycles
   while(1){
-    char c = uart_polling_get_byte();
-    uart_polling_put_byte(c);
+    if(counter == 10000){
+      int button1 = gpio_read(GPIO_B, 10);
+      int button2 = gpio_read(GPIO_B, 5);
+      printk("button 1 = %d   button 2 = %d", button1, button2);
+      counter = 0;
+    }
+    counter++;
+    //char c = uart_polling_get_byte();
+    //uart_polling_put_byte(c);
   }
   return 0;
 }
